8f2ce7c3c1d1fcbb2baee5ccdf35914b7a94563a
diesel2021 <asmeenk@planet.nl>
2021-12-27 17:36:04 +0100

    Added test_rtic_i2c_slave example

--- nucleo-G070/examples/test_rtic_i2c_slave.rs
+++ nucleo-G070/examples/test_rtic_i2c_slave.rs
@@ -4,6 +4,29 @@
 
 use rtic::app;
 
+macro_rules! checkIsWrite {
+    ($txd:ident, $direction:ident) => {
+        match $direction {
+            MasterWriteSlaveRead => (),
+            _ => {
+                write!($txd, "Error incorrect direction {:?}\r", $direction).unwrap();
+                return;
+            }
+        }
+    };
+}
+macro_rules! checkIsRead {
+    ($txd:ident, $direction:ident) => {
+        match $direction {
+            MasterReadSlaveWrite => (),
+            _ => {
+                write!($txd, "Error incorrect direction {:?}\r", $direction).unwrap();
+                return;
+            }
+        }
+    };
+}
+
 #[app(device = hal::stm32, peripherals = true)]
 mod app {
     extern crate rtic;
@@ -28,7 +51,7 @@ mod app {
     use hal::timer::pins::TimerPin;
     use hal::stm32::{TIM17,TIM15};
     use hal::serial::{Serial, Tx, Rx,  FullConfig, FifoThreshold}; 
-    use hal::i2c::{Error, Config, I2c, I2cControl, I2cMaster, I2cResult};
+    use hal::i2c::{Error, Config, I2c, I2cControl, I2cSlave, I2cResult, SlaveAddressMask,I2cDirection};
 
     use nb::block;
 
@@ -46,6 +69,8 @@ mod app {
         tim17: Timer<TIM17>,
         blink: PA5<Output<PushPull>>,
         delay: Delay<TIM15>,
+        count:usize,
+        errors:usize,
     }
 
     #[init()]
@@ -66,7 +91,10 @@ mod app {
 
         let sda = gpiob.pb9.into_open_drain_output();
         let scl = gpiob.pb8.into_open_drain_output();
-        let configi2c = hal::i2c::Config::with_timing(0x2020_151b);
+        let mut configi2c = hal::i2c::Config::with_timing(0x2020_151b);
+        configi2c.slave_address(0x30);
+        configi2c.slave_address_2(0x40, SlaveAddressMask::MaskFourBits);
+    
         let mut i2c = ctx.device.I2C1.i2c(sda, scl, configi2c, &mut rcc);
         i2c.listen();
 
@@ -87,7 +115,7 @@ mod app {
         let (mut txd, mut rxd) = serial.split(); 
 
         writeln!(txd,
-            "RTIC driven non Blocking i2c master test. Should be used together with one of test_*_slave tests\r"
+            "RTIC driven non Blocking i2c slave test. Should be used together with one of test_*_master tests\r"
             ).unwrap();        
 
         let mut counter:usize = 1;
@@ -102,119 +130,26 @@ mod app {
             tim17,
             blink,
             delay,
+            errors:0,
+            count:0,
         }, 
         init::Monotonics())
     }
 
-    #[idle(local = [delay],  shared = [counter, i2c, txd ])]
+    #[idle(local = [delay],  shared = [counter, txd ])]
     fn idle(ctx: idle::Context) -> ! {
         // Locals in idle have lifetime 'static
         let counter = ctx.shared.counter;
         let mut txd = ctx.shared.txd;
-        let mut i2c = ctx.shared.i2c;
         let mut delay = ctx.local.delay;
-
-        let mut buf_long: [u8; 50] = [0; 50]; //
-        let mut buf_short: [u8; 20] = [0; 20]; //
-        let mut buf_rcv: [u8; 20] = [0; 20]; //
-        let mut buf_10: [u8; 10] = [0; 10]; //
-        let mut buf_io = [0_u8;1];
     
 
         let mut ready = false;
         loop {
-            txd.lock(|txd | { writeln!(txd, "\r\rStart of test\r").unwrap(); });
-
-            // test 1: slave address 0x61 should not be addressable
-            for i in 0..buf_short.len() {
-                buf_short[i] = 0x41 + (i as u8)
-            }
-            
-            while !ready {
-                i2c.lock(|i2c| {
-                    ready =  ! matches!( i2c.master_write(0x61, &buf_short), Err(nb::Error::WouldBlock))  
-                });
-            }
-            // 0x41 good case master write slave read: master does send 20 bytes slave receives 20 bytes
-            ready = false; delay.delay(10.ms());
-            while !ready {
-                i2c.lock(|i2c| {
-                    ready =  ! matches!( i2c.master_write(0x41, &buf_short), Err(nb::Error::WouldBlock))  
-                });
-            }
-            // 0x42 bad case master write slave read: master does send less than 20 bytes
-            for i in 0..buf_10.len() {
-                buf_10[i] = 0x20 + (i as u8)
-            }
-            ready = false; delay.delay(10.ms());
-
-            while !ready {
-                i2c.lock(|i2c| {
-                    ready =  ! matches!( i2c.master_write(0x42, &buf_10), Err(nb::Error::WouldBlock) );
-                });
-            }
-            // 0x43 bad case master write slave read: master does send more than 20 bytes, slave does NACK
-            for i in 0..buf_long.len() {
-                buf_long[i] = 0x61 + (i as u8)
-            }
-            ready = false; delay.delay(10.ms());
-            while !ready {
-                i2c.lock(|i2c| {
-                    ready =  ! matches!( i2c.master_write(0x43, &buf_long) , Err(nb::Error::WouldBlock) );  
-                });
-            }
-            // 0x44 master write_read good case: master sends and expects 20 bytes
-            for i in 0..buf_short.len() {
-                buf_short[i] = 0x41 + (i as u8)
-            }
-            ready = false; delay.delay(10.ms());
-            while !ready {
-                i2c.lock(|i2c| {
-                    ready =  ! matches!( i2c.master_write_read(0x44, &buf_short, 20), Err(nb::Error::WouldBlock) );  
-                });
-            }
-            // 0x48 master read slave write good case: exact 20 characters
-            for i in 0..buf_short.len() {
-                buf_short[i] = 0x61 + (i as u8)
-            }
-            ready = false; delay.delay(10.ms());
-            while !ready {
-                i2c.lock(|i2c| {
-                    ready =  ! matches!( i2c.master_read(0x48, 20) , Err(nb::Error::WouldBlock) );  
-                });
-            }
-            // 0x49 master read slave write bad  case: master expects 50 slave does send 20 characters
-            for i in 0..buf_long.len() {
-                buf_long[i] = 0x61 + (i as u8)
-            }
-            ready = false; delay.delay(10.ms());
-            while !ready {
-                i2c.lock(|i2c| {
-                    ready =  ! matches!( i2c.master_read(0x49, 50), Err(nb::Error::WouldBlock) );  
-                });
-            }
-            // 0x4A master read slave write bad  case: master expects 20 does slave does send 50 characters
-            for i in 0..buf_short.len() {
-                buf_short[i] = 0x41 + (i as u8)
-            }
-            ready = false; delay.delay(10.ms());
-            while !ready {
-                i2c.lock(|i2c| {
-                    ready =  ! matches!( i2c.master_read(0x4A, 20), Err(nb::Error::WouldBlock))  
-                });
-            }
-            // 0x4F test end and slave will present results
-            let mut result: [u8; 2] = [0, 0];
-            ready = false; delay.delay(10.ms());
-            while !ready {
-                i2c.lock(|i2c| {
-                    ready =  ! matches!( i2c.master_read(0x4f, 2) , Err(nb::Error::WouldBlock))
-                });
-            }
+            txd.lock(|txd | { writeln!(txd, "\r\rHello I am alive\r").unwrap(); });
             delay.delay(10_000.ms());
         }
     }
-
 
     #[task(binds = TIM17,  priority = 3, local = [tim17, blink], shared = [counter,i2c ])]
     fn timer17(ctx: timer17::Context) {
@@ -225,20 +160,17 @@ mod app {
         let mut buf_short = [0_u8;1];
         (i2c, counter ).lock(|i2c, counter| {
             *counter += 1;
-            
-            buf_short[0] = *counter as u8;
-            // ignore result ignore failure due to i2c bus busy
-            // concurrent acces does not work yet, internal state gets confused
-            //let _ = i2c.master_write(0x21, &buf_short);  
         });
         ctx.local.tim17.clear_irq();
 
     } // tim 17
 
-    #[task(binds = I2C1,  priority = 4, local = [], shared = [i2c, txd ])]
+    #[task(binds = I2C1,  priority = 4, local = [errors,count], shared = [i2c, txd ])]
     fn i2_irq(ctx: i2_irq::Context) {
         let mut txd = ctx.shared.txd;
         let mut i2c = ctx.shared.i2c;
+        let mut errors = ctx.local.errors;
+        let mut count = ctx.local.count;
 
         (i2c, txd).lock(|i2c, txd| {
             let address = i2c.get_address();
@@ -246,7 +178,7 @@ mod app {
                 Ok( b) => {
                     match b {
                     I2cResult::Addressed(ad, dir) => {
-                            writeln!(txd, "Addressed {:?} dir {:?}\r", ad, dir ).unwrap();
+                            addressed_case(txd, i2c as &mut I2cSlave, ad, dir, count,errors);
                         },     
                     I2cResult::Data(d) => 
                         // prevent too many output from the i2c extender output
@@ -255,17 +187,103 @@ mod app {
                                 write!(txd, "{}", d[i] as char).unwrap();
                             }
                             writeln!(txd, "!\r").unwrap();
-                            irq_ok_case(txd, i2c as &mut I2cMaster, address);
+                            irq_ok_case(txd, i2c as &mut I2cSlave, address);
                         }
                     }
                 },
                 Err(nb::Error::WouldBlock) => (), // ignore the WouldBlock error
-                Err(err) => irq_bad_case(txd, i2c as &mut I2cMaster, address, err),
+                Err(err) => {
+                    *errors += 1;
+                    irq_bad_case(txd, i2c as &mut I2cSlave, address, err)
+                },
             }
         });
     } // I2C
 
-    fn irq_ok_case(txd: &mut Tx<USART2,FullConfig>, i2c:  &dyn I2cMaster, address:u16) {
+    fn addressed_case(txd: &mut Tx<USART2,FullConfig>, i2c:  &mut dyn I2cSlave, address:u16, dir:I2cDirection, count:&mut usize, errors:&mut usize){
+        writeln!(txd, "Addressed {:x} dir {:?}\r", address, dir ).unwrap();
+        let mut buf_long: [u8; 50] = [0; 50]; //
+        let mut buf_short: [u8; 20] = [0; 20]; //
+
+        *count += 1;    
+        match address {
+            0x41 => {
+                // 0x41 good case master write slave read: master does send 20 bytes slave receives 20 bytes
+                checkIsWrite!(txd, dir);
+                let _ = i2c.slave_read(20); 
+            },
+            0x42 => {
+                // 0x42 bad case master write slave read: master does send less than 20 bytes
+                checkIsWrite!(txd, dir);
+                let _ = i2c.slave_read(10);
+            },
+            0x43 => {
+                // 0x43 bad case master write slave read: master does send more than 20 bytes, slave does NACK
+                checkIsWrite!(txd, dir);
+                let _ = i2c.slave_read(20);
+            },
+            0x44 => {
+                // 0x44 master read write good case
+                match dir {
+                    I2cDirection::MasterWriteSlaveRead => {
+                        i2c.slave_sbc(false);
+                        let _ = i2c.slave_read(20); 
+                    },
+                    I2cDirection::MasterReadSlaveWrite => {
+                        i2c.slave_sbc(true);
+                        let buf_short = [1_u8;20];
+                        let _ = i2c.slave_write(&buf_short); 
+                    },
+                    _ => {
+                        writeln!(txd, "UU\r").unwrap();
+                    }
+                };
+            },
+            0x48 => {
+                // 0x48 master read slave write good case: exact 20 characters
+                for i in 0..buf_short.len() {
+                    buf_short[i] = 0x61 + (i as u8)
+                }
+                checkIsRead!(txd, dir);
+                let _ = i2c.slave_write(&buf_short);
+            },
+            0x49 => {
+                // 0x49 master read slave write bad  case: master expects 50 does slave does send 20 characters
+                checkIsRead!(txd, dir);
+                let _ = i2c.slave_write(&buf_short); 
+            },
+            0x4A => {
+                // 0x4A master read slave write bad  case: master expects 20 does slave does send 50 characters
+                checkIsRead!(txd, dir);
+                let _ =  i2c.slave_write(&buf_long);
+                
+            },
+            0x4B => {
+                // 0x4B master write_read good case each 20 chars
+                checkIsRead!(txd, dir);
+                let _ = i2c.slave_write(&buf_long); 
+                
+            },
+            0x4F => {
+                checkIsRead!(txd, dir);
+                let result: [u8; 2] = [0,0];
+                let _ =  i2c.slave_write(&result); 
+                writeln!(
+                    txd,
+                    "Test finished. nr tests/nr errors: {}/{}!\r",
+                    *count , *errors
+                )
+                .unwrap();
+                writeln!(txd, "-----\r").unwrap();
+                *count = 0;
+                *errors = 0;
+            }
+            _ => (),
+        }
+
+    }
+
+    fn irq_ok_case(txd: &mut Tx<USART2,FullConfig>, i2c:  &dyn I2cSlave, address:u16) {
         match address {
             // 0x20 i2c IO extender with 8 leds
             0x20 => (),
@@ -323,7 +341,7 @@ mod app {
         }
     }
     
-    fn irq_bad_case(txd: &mut Tx<USART2,FullConfig>, i2c:  &dyn I2cMaster, address:u16, err:nb::Error<Error>) {
+    fn irq_bad_case(txd: &mut Tx<USART2,FullConfig>, i2c:  &dyn I2cSlave, address:u16, err:nb::Error<Error>) {
         match address {
         // 0x20 i2c IO extender with 8 leds
         0x20 => writeln!(txd, "Test 0x20 i2c extender not found got error: {:?}\r", err).unwrap(),
